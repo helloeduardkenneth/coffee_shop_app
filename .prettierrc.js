85ced0371323a3ca0ed29edee3837e8842538bb0
helloeduardkenneth <personal.eduardkennethgaluran@gmail.com>
2024-07-11 00:42:23 +0800

    init: added assets, source files, react-native.config.js file

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,14 @@
 module.exports = {
+  tabWidth: 4,
+  printWidth: 80,
+  useTabs: false,
+  semi: false,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  endOfLine: 'auto',
 };
